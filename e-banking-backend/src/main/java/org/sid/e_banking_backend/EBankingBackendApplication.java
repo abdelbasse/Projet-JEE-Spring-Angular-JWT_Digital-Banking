ffe17d06fd14343b16d59d434ceae02b12c9069a
abdelbasse <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-04-28 09:38:48 +0100

    project init

---
+++ e-banking-backend/src/main/java/org/sid/e_banking_backend/EBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sid.e_banking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankingBackendApplication.class, args);
+	}
+
+}
---
+++ e-banking-backend/src/test/java/org/sid/e_banking_backend/EBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.e_banking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
