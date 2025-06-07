bf1375d8d7914b73b8d65a796dc586f1d9f222ce
abdelbasse <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-06-07 21:30:26 +0100

    fix fontend - Add Interceptor

---
+++ e-banking-fontend/src/app/interceptors/app-http.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AppHttpInterceptor implements HttpInterceptor {
+
+  constructor(private authService : AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let req = request.clone().headers.set('Authorization', `Bearer ${this.authService.accessToken}`);
+    return next.handle(request);
+  }
+}
