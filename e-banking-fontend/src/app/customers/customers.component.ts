90e9b0b6f709bda3068458bf54bb56d0b5b68dc9
abdelbasse <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-06-07 23:16:46 +0100

    fix fontend - finish customer search design

--- e-banking-fontend/src/app/customers/customers.component.ts
+++ e-banking-fontend/src/app/customers/customers.component.ts
@@ -12,22 +12,31 @@ import {Router} from "@angular/router";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers! : Observable<Array<Customer>>;
+  customers!: Observable<Array<Customer>>;
   errorMessage!: string;
-  searchFormGroup : FormGroup | undefined;
-  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) { }
+  searchFormGroup: FormGroup | undefined;
+
+  // Only totalCustomers is dynamic
+  totalCustomers: number = 0;
+
+  constructor(private customerService: CustomerService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    this.searchFormGroup=this.fb.group({
-      keyword : this.fb.control("")
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control("")
     });
     this.handleSearchCustomers();
   }
+
   handleSearchCustomers() {
-    let kw=this.searchFormGroup?.value.keyword;
-    this.customers=this.customerService.searchCustomers(kw).pipe(
+    let kw = this.searchFormGroup?.value.keyword;
+    this.customers = this.customerService.searchCustomers(kw).pipe(
+      map(customers => {
+        this.totalCustomers = customers.length;
+        return customers;
+      }),
       catchError(err => {
-        this.errorMessage=err.message;
+        this.errorMessage = err.message;
         return throwError(err);
       })
     );
@@ -35,24 +44,18 @@ export class CustomersComponent implements OnInit {
 
   handleDeleteCustomer(c: Customer) {
     let conf = confirm("Are you sure?");
-    if(!conf) return;
+    if (!conf) return;
     this.customerService.deleteCustomer(c.id).subscribe({
-      next : (resp) => {
-        this.customers=this.customers.pipe(
-          map(data=>{
-            let index=data.indexOf(c);
-            data.slice(index,1)
-            return data;
-          })
-        );
+      next: () => {
+        this.handleSearchCustomers(); // Reload and update stats
       },
-      error : err => {
+      error: err => {
         console.log(err);
       }
     })
   }
 
   handleCustomerAccounts(customer: Customer) {
-    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
+    this.router.navigateByUrl("/customer-accounts/" + customer.id, { state: customer });
   }
 }
